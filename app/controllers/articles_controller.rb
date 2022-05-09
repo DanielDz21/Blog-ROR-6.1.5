960b593c7840e8b9866a7ba73c6083186378e793
DanielDz21 <daniel.da.silva.tj2011@gmail.com>
2022-05-09 09:21:01 -0300

    arrumando problema de versao do rails

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -41,7 +41,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.destroy
 
-    redirect_to root_path
+    redirect_to root_path, status: :see_other
   end
 
   private
